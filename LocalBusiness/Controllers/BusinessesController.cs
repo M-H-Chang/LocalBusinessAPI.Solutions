49533328a3835d2e6faf5b2e58c2268c55e2d7f2
Min Chang <minchangmhc@gmail.com>
2021-06-11 13:22:44 -0700

    🚧 controller in progress.

--- LocalBusiness/Controllers/BusinessesController.cs
+++ LocalBusiness/Controllers/BusinessesController.cs
@@ -26,5 +26,64 @@ namespace LocalBusiness.Controllers
         query = query.Where(entry => entry.Name == name);
       }
     }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Business>> GetBusiness(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+      return business;
+    }
+    [HttpPost]
+    public async Task<ActionResult<Business>> Post(Business business)
+    {
+      _db.Businesses.Add(business);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction("Post", new { id = business.BusinessId }, business);
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(business).State = EntityState.Modified;
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool BusinessExists(int id)
+    {
+      return _db.Businesses.Any(e => e.BusinessId == id);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+      _db.Businesses.Remove(business);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+
   }
 }
