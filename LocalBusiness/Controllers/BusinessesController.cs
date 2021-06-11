f85539037137012fb4d4a51653328aee29ae4919
Min Chang <minchangmhc@gmail.com>
2021-06-11 11:16:28 -0700

    ✨ Add migrations to the file as well as update controller.

--- LocalBusiness/Controllers/BusinessesController.cs
+++ LocalBusiness/Controllers/BusinessesController.cs
@@ -4,3 +4,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LocalBusiness.Models;
 using System.Linq;
+
+namespace LocalBusiness.Controllers
+{
+  [Route("api/controller")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private readonly LocalBusinessContext _db;
+    public BusinessesController(LocalBusinessContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Business>>> Get(string name)
+    {
+      var query = _db.Businesses.AsQueryable();
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+    }
+  }
+}
---
+++ LocalBusiness/Migrations/20210611172906_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusiness.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Businesses",
+                columns: table => new
+                {
+                    BusinessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Location = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Businesses", x => x.BusinessId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Businesses",
+                columns: new[] { "BusinessId", "Location", "Name" },
+                values: new object[] { 1, null, "Pho Than Brothers" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Businesses");
+        }
+    }
+}
